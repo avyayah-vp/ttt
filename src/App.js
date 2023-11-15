9a4217eeec7e53bbc490ceb601558556e925ae55
Vishnu Prakash <vishnuash1717@gmail.com>
2023-11-15 11:55:32 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import NavBar from './components/navbar';
 import HeroSection from './components/hero';
+import Main from './components/main';
 function App() {
   return (
     <>
     <NavBar/>
     <HeroSection/>
+    <Main/>
     </>
   );
 }
---
+++ src/components/main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Main extends React.Component {
+    render() {
+        return (
+            <div style={{ marginTop: '15vh', padding: '40px' }}>
+                <h1>Changing World Order</h1>
+                <p> The world is changing at a feverish pace. Friends,
+                    colleagues and everyone engaged in knowledge
+                    work are beginning to sense this, including ...</p>
+                <p><span style={{color: 'blue'}}>musing</span> by anujgosalia</p>
+            </div>
+        );
+    }
+}
+
+export default Main;
